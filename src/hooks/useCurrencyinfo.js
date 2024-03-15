900bf2bc8cf2bf33d05a1ba96c3243c86096548f
Sagar Gupta <sagarrock99@gmail.com>
2024-03-16 01:08:51 +0530

    Api Fetch

---
+++ src/hooks/useCurrencyinfo.js
@@ -0,0 +1,12 @@
+import { useEffect,useState } from "react";
+const url=`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+
+function useCurrencyInfo(curr){
+    const [data,setData]=useState({})
+    useEffect(()=>{
+        fetch(url)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+    },[currency])
+}
+ 
