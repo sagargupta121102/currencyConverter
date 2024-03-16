e892b543a3f2ab43b60ff8bd9f4ad8e84deb7054
Sagar Gupta <sagarrock99@gmail.com>
2024-03-16 23:46:51 +0530

    Error Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {InputBox} from './components'
-import useCurrencyInfo from './hooks/useCurrencyInfo'
+import useCurrencyInfo from './hooks/UseCurrencyInfo'
 
 
 function App() {
